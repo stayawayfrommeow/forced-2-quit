bb9a404c6ced1b44efa4c858c8fac8b0e43ad379
Efim <101406592+stayawayfrommeow@users.noreply.github.com>
2025-11-02 15:52:08 +0300

    basic loop established

---
+++ day1/day_1.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@onready var quest1 = $Interactable
+@onready var quest2 = $Interactable2
+@onready var quest3 = $Interactable3
+@onready var win_screen = $win_screen_placeholder
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	quest1.visible = true
+	quest2.visible = false
+	quest3.visible = false
+	win_screen.visible = false
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#quest 1 completed
+func _on_interactable_tree_exited():
+	quest2.visible = true
+	quest3.visible = false
+	pass # Replace with function body.
+
+#quest 2 completed
+func _on_interactable_2_tree_exited():
+	quest3.visible = true
+	pass # Replace with function body.
+
+#quest 3 completed
+func _on_interactable_3_tree_exited():
+	win_screen.visible = true
+	get_tree().change_scene_to_file("res://day_passed/day_passed.tscn")
+	pass # Replace with function body.
---
+++ interactables/interactable.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+#тут инициация переменных сцены
+@onready var interact_popup = $Interact_popup
+@onready var gj_popup = $gj_popup
+@onready var animations = $Interact_popup/AnimationPlayer
+@onready var player_near = false
+@onready var obj_sprt = $AnimatedSprite2D
+@onready var killtimer = $kill_timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#обект видно
+	obj_sprt.visible = true
+	
+	#по умолчанию попапы скрыты
+	interact_popup.visible = false
+	gj_popup.visible = false
+	
+	
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event: InputEvent): 
+	if (player_near and event.is_action_pressed("interact")):
+		gj_popup.visible = true
+		obj_sprt.visible = false
+		interact_popup.visible = false
+		killtimer.start(1)
+	pass
+
+func _on_area_2d_body_entered(body):
+	#если плеер подошёл, то показываем попап
+	player_near = true
+	interact_popup.visible = true
+	animations.play("popup_enter")
+	
+	
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	#если плеер отошёл, то скрываем попап
+	player_near = false
+	animations.play("popup_escape")
+	
+	pass # Replace with function body.
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "popup_escape":
+			interact_popup.visible = false
+			
+	pass # Replace with function body.
+
+
+func _on_kill_timer_timeout():
+	queue_free()
+	pass # Replace with function body.
